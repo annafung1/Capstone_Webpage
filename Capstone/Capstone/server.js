3fda145eb0995bebb0b71718a1240dfffb985b27
Monika Tsung <monikaylt@gmail.com>
2023-03-31 15:11:20 -0400

    Added water sensor

--- Capstone/Capstone/server.js
+++ Capstone/Capstone/server.js
@@ -48,7 +48,11 @@ http.createServer(function (req, res) {
             res.end(fileContents);
         }
 
-
+        else if (url === '/water') {
+            var fileContents = fs.readFileSync('./Water.html', { encoding: 'utf8' });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(fileContents);
+        }
         else {
             res.writeHead(404, { 'Content-Type': 'text/plain' });
             res.end('404 Not Found');
